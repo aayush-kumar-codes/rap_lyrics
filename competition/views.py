98552605d1d72256e27d5d7cf386142bb628c3b0
aayush <aayush.excel2011@gmail.com>
2023-04-05 19:11:02 +0530

    Added new competition page template

--- competition/views.py
+++ competition/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def list_all_competition(request):
     return render(request=request, template_name='competition/competition.html')
+
+
+def new_competition(request):
+    return render(request=request, template_name='competition/new-competition.html')
